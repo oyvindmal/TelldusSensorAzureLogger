15a0ecd46af3aad6c48433154e88eb0fbc0e29b4
Øyvind Malin <oyvind@epostboks.org>
2014-05-22 19:04:57 -0400

    Hopefully finished tellduslogic

--- testclients/SendTelldusEvents.js
+++ testclients/SendTelldusEvents.js
@@ -1,3 +1,9 @@
 var eventParser = require("../lib/TelldusEventParser.js");
-
+console.log("FineOffset")
 eventParser.SendData("16:TDRawDeviceEvent68:class:sensor;protocol:fineoffset;id:181;model:temperature;temp:28.4;i1s");
+console.log("Mandolyn")
+eventParser.SendData("16:TDRawDeviceEvent85:class:sensor;protocol:mandolyn;id:12;model:temperaturehumidity;temp:28.4;humidity:48;i1s");
+console.log("Random garbage");
+eventParser.SendData("This is not a sensor");
+console.log("Not a sensor but looks accurate")
+eventParser.SendData("16:TDRawDeviceEvent85:class:sensffor;protocol:mandolyn;id:12;model:temperaturehumidity;temp:28.4;humidity:48;i1s");
---
+++ testclients/TestLiveData.js
@@ -0,0 +1,19 @@
+var net = require('net');
+var EventParser = require("./lib/TelldusEventParser.js");
+// Set up a connection to the TelldusEvents socket
+var conn = net.createConnection('/tmp/TelldusEvents');
+ 
+// Set the encoding so that you get data that is actually readable by humans
+conn.setEncoding('utf-8');
+ 
+//Eventlistener on connection
+conn.on('connect' , function () {
+        console.log("Connected");
+});
+ 
+ 
+//Eventlistener on data recieved
+conn.on('data', function(data) {
+        //Log the data to the console
+        EventParser.SendData(data);
+});
